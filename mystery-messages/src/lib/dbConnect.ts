f91e2b94d9e8abf14eb02421abf9d1f1264dffe1
saranzafar <saranzafar736@gmail.com>
2024-07-21 12:27:16 +0500

    connection to db

---
+++ mystery-messages/src/lib/dbConnect.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+    isconnected?: number
+}
+
+const connection: ConnectionObject = {}
+
+async function dbConnect(): Promise<void> {
+    if (connection.isconnected) {
+        console.log("Already connected to database");
+        return
+    }
+    try {
+        const db = await mongoose.connect(process.env.MONGODB_URI || "", {})
+        connection.isconnected = db.connections[0].readyState
+        console.log("DB Connected Successfully", db)
+    } catch (error) {
+        console.log("DB connection failed", error)
+        process.exit(1)//gracefully exit 
+    }
+}
+
+export default dbConnect
