5a92b4be32c6e04975cd5b96e54f168383f715c5
saranzafar <saranzafar736@gmail.com>
2024-08-16 22:44:03 +0500

    flowing

--- mystery-messages/src/api/sign-up/route.ts
+++ mystery-messages/src/api/sign-up/route.ts
@@ -9,7 +9,70 @@ export async function POST(reqest: Request) {
 
     try {
         const { username, email, password } = await reqest.json()
-        
+        const existingUserVerifiedByUserName = await userModel.findOne({
+            username,
+            isVerified: true
+        });
+
+        if (existingUserVerifiedByUserName) {
+            return Response.json({
+                success: false,
+                message: "User name is already taken"
+            }, { status: 400 })
+        }
+
+        const existingUserByEmail = await userModel.findOne({ email })
+        const verifyCode = Math.floor(100000 + Math.random() * 900000).toString()
+
+        if (existingUserByEmail) {
+            if (existingUserByEmail.isVerified) {
+                return Response.json({
+                    success: false,
+                    message: "User already exist with email"
+                }, { status: 400 })
+            } else {
+                const hashedPassword = await bcrypt.hash(password, 10)
+                existingUserByEmail.password = hashedPassword
+                existingUserByEmail.verifyCode = verifyCode
+                existingUserByEmail.verifyCodeExpiry = new Date(Date.now() + 3600000)
+            }
+            await existingUserByEmail.save()
+        } else {
+            const hashedPassword = await bcrypt.hash(password, 10)
+            const expiryDate = new Date()
+            expiryDate.setHours(expiryDate.getHours() + 1)
+
+            const newUser = new userModel({
+                username,
+                email,
+                password: hashedPassword,
+                verifyCode,
+                verifyCodeExpiry: expiryDate,
+                isVerified: false,
+                isAcceptingMessage: true,
+                messages: [],
+            })
+            await newUser.save()
+        }
+
+        // send verification email
+        const emailResponse = await sendVerificationEmail(
+            email,
+            username,
+            verifyCode
+        )
+        if (!emailResponse.success) {
+            return Response.json({
+                success: false,
+                message: emailResponse.message
+            }, { status: 500 })
+        }
+
+        return Response.json({
+            success: true,
+            message: "User registered successfully. Please verify your email"
+        }, { status: 201 })
+
     } catch (error) {
         console.error("Error Registring User");
         return Response.json({
