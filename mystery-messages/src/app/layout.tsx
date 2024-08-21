7c9cb6197d63dd70ab3cb98b65ed7d65a06e9c39
saranzafar <saranzafar736@gmail.com>
2024-08-21 11:54:46 +0500

    Navbar and message card with bug fixes in Nextjs

--- mystery-messages/src/app/layout.tsx
+++ mystery-messages/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "../globals.css";
+import "./globals.css";
+import AuthProvider from "@/context/AuthProvider";
+import { Toaster } from "@/components/ui/toaster"
 import Navbar from "@/components/Navbar";
 
 
@@ -18,10 +20,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <AuthProvider>
         <body className={inter.className}>
           <Navbar />
           {children}
+          <Toaster />
         </body>
+      </AuthProvider >
     </html>
   );
 }
---
+++ mystery-messages/src/components/Navbar.tsx
@@ -0,0 +1,35 @@
+"use client"
+
+import React from 'react'
+import Link from "next/link"
+import { useSession, signOut } from 'next-auth/react'
+import { User } from "next-auth"
+import { Button } from './ui/button'
+
+function Navbar() {
+    const { data: session } = useSession()
+    const user: User = session?.user as User
+
+
+    return (
+        <nav className='p-4 md:p-6 shadow-md'>
+            <div className='container mx-auto flex flex-col md:flex-row justify-between items-center'>
+                <a href="#" className='text-xl font-bold mb-4 md:mb-0'>Mystry Messages</a>
+                {
+                    session ? (
+                        <>
+                            <span className='mr-4'>User {user?.username || user?.email}</span>
+                            <Button onClick={() => signOut()} className='w-full md:w-auto'>Logout</Button>
+                        </>
+                    ) : (
+                        <Link href={"/sign-in"}>
+                            <Button className='w-full md:w-auto'>Login</Button>
+                        </Link>
+                    )
+                }
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
